239cb3e715f8d0afed5cf7680b90a11ce42614da
kydyrbekova <a.kydyrbekova@gmail.com>
2020-01-23 16:21:29 +0600

    commas

--- ascii-fs_test.go
+++ ascii-fs_test.go
@@ -1,40 +1,63 @@
 package main
 
 import (
-	"io/ioutil"
 	"testing"
-	)
+)
 
 func TestReadFile(t *testing.T) {
 	var tests = []struct {
-		input []string
-		output string
+		input    []string
+		output   string
 		expected string
-	} {
-		{"First\nTest" shadow --output=test00.txt, test00.txt, exp00.txt },
-		{"hello" standard --output=test01.txt, test01.txt, exp01.txt},
-		{"123 -> #$%" standard --output=test02.txt, test02.txt, exp02.txt},
-		{"432 -> #$%&@" shadow --output=test03.txt, test03.txt, exp03.txt},
-		{"There" shadow --output=test04.txt, test04.txt, exp04.txt},
-		{"123 -> \"#$%@" thinkertoy --output=test05.txt, test05.txt, exp05.txt},
-		{"2 you" thinkertoy --output=test06.txt, test06.txt, exp06.txt},
-		{"Testing long output!" standard --output=test07.txt, test07.txt, exp07.txt},
+	}{
+		{[]string{"First\nTest", "shadow", "--output=test00.txt"},
+			test00.txt,
+			exp00.txt,
+		},
+		{[]string{"hello", "standard", "--output=test01.txt"},
+			test01.txt,
+			exp01.txt,
+		},
+		{[]string{"123 -> #$%", "standard", "--output=test02.txt"},
+			test02.txt,
+			exp02.txt,
+		},
+		{[]string{"432 -> #$%&@", "shadow", "--output=test03.txt"},
+			test03.txt,
+			exp03.txt,
+		},
+		{
+			[]string{"There", "shadow", "--output=test04.txt"},
+			test04.txt,
+			exp04.txt,
+		},
+		{[]string{"123 -> \"#$%@", "thinkertoy", "--output=test05.txt"},
+			test05.txt,
+			exp05.txt,
+		},
+		{[]string{"2 you", "thinkertoy", "--output=test06.txt"},
+			test06.txt,
+			exp06.txt,
+		},
+		{[]string{"Testing long output!", "standard", "--output=test07.txt"},
+			test07.txt,
+			exp07.txt,
+		},
 	}
 
-		for _, test:= range tests {
-			Output(test.input)
-			dataoutput, err := ioutill.ReadFile(test.output)
-			if err!=nil {
-				t.Fatal("Could not open output file")
-			}
-			dataexp, errexp := ioutill.ReadFile(test.expected)
-			if errexp!=nil {
-				t.Fatal("Could not open expected file")
-			}
-			if string(dataoutput) != dataexp {
-				t.Fatal("Output do not match expected")
-			}
+	for _, test := range tests {
+		Output(test.input)
+		dataoutput, err := ioutill.ReadFile(test.output)
+		if err != nil {
+			t.Fatal("Could not open output file")
 		}
-
+		dataexp, errexp := ioutill.ReadFile(test.expected)
+		if errexp != nil {
+			t.Fatal("Could not open expected file")
+		}
+		if string(dataoutput) != dataexp {
+			t.Fatal("Output do not match expected")
+		}
+	}
 
 }
